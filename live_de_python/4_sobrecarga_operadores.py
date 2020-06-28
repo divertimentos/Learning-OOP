fd16d2150ca042ddbf5bc215df701fe344ec7c8b
guiemi <guiemi.teixeira@gmail.com>
2020-06-28 15:37:19 -0300

    Finaliza as anotações da live 4 de OOP

--- live_de_python/4_sobrecarga_operadores.py
+++ live_de_python/4_sobrecarga_operadores.py
@@ -1,3 +1,5 @@
+# Operadores unários
+
 class Dois:
     val = 2
     def __neg__(self):
@@ -27,3 +29,40 @@ class Ponto:
 
     def __repr__(self):
         return f"Ponto({self.x}, {self.y})"
+
+
+# Operadores infixos
+
+class Numero:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('__add__')
+        return self.val + other
+
+    def __radd__(self, other):
+        print('__radd__')
+        return self.val + other
+
+# Lista bem loka
+
+class ListaBemLoka(list):
+    def __add__(self, val):
+        """Soma todos os itens da lista com val"""
+        return ListaBemLoka([x + val for x in self])
+
+    def __lshift__(self, val):
+        """Fazer append na lista usando <<"""
+        self.append(val)
+        
+    def __rshift__(self, pos):
+        """Remove um item com >>"""
+        return self.pop(pos)
+
+    def __neg__(self):
+        return ListaBemLoka(reversed(self))
+    
+    def __iadd__(self, val):
+        """Toma toos os itens da lista com val e manter no objeto"""
+        self = ListaBemLoka([x + val for x in self])
